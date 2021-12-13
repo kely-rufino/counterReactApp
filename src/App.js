178b0bd159eb79a2984305afd53c329126a70a7b
Kely Rufino <87963324+kely-rufino@users.noreply.github.com>
2021-12-13 09:59:54 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Counter from './components/Counter'
+import Multiply from "./components/Multiply";
+
+
+function App() {
+    return (
+        <div>
+            <Counter/>
+            <Multiply/>
+        </div>
+    );
+}
+
+export default App;
